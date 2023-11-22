65d4e025aaa278b677ef4e694936f64be8030ba0
Dragoș Petrașcu <dragos_petrascu@yahoo.com>
2023-11-22 17:05:43 +0200

    extract getMomentOfDay

--- src/main/java/ro/fortech/practice/GreetingMessageGenerator.java
+++ src/main/java/ro/fortech/practice/GreetingMessageGenerator.java
@@ -1,7 +1,34 @@
 package ro.fortech.practice;
 
+import java.util.Calendar;
+
 public class GreetingMessageGenerator {
+    public String generateGreetingMessage() {
+        return generateGreetingMessage(null);
+    }
+
     public String generateGreetingMessage(String personName) {
-        return "Hello, " + personName + "!";
+        if (personName == null) {
+            return "Good " + getMomentOfDay() + ", stranger!";
+        }
+        return "Good " + getMomentOfDay() + ", " + personName + "!";
+    }
+
+    private String getMomentOfDay() {
+        int hourOfDay = getHourOfDay();
+        if ((5 <= hourOfDay) && (hourOfDay < 12)) {
+            return "morning";
+        }
+        if ((12 <= hourOfDay) && (hourOfDay < 18)) {
+            return "afternoon";
+        }
+        if ((18 <= hourOfDay) && (hourOfDay < 22)) {
+            return "evening";
+        }
+        return "night";
+    }
+
+    private int getHourOfDay() {
+        return Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
     }
 }
