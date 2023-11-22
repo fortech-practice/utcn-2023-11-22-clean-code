2cafeeb3d6b33872f0efe4b523892ccbf771361d
Dragoș Petrașcu <dragos_petrascu@yahoo.com>
2023-11-22 16:51:38 +0200

    add GreetingMessageGenerator

---
+++ src/main/java/ro/fortech/practice/GreetingMessageGenerator.java
@@ -0,0 +1,7 @@
+package ro.fortech.practice;
+
+public class GreetingMessageGenerator {
+    public String generateGreetingMessage(String personName) {
+        return "Hello, " + personName + "!";
+    }
+}
--- src/main/java/ro/fortech/practice/Main.java
+++ src/main/java/ro/fortech/practice/Main.java
@@ -2,6 +2,8 @@ package ro.fortech.practice;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GreetingMessageGenerator generator = new GreetingMessageGenerator();
+        String message = generator.generateGreetingMessage("Buggs Bunny");
+        System.out.println(message);
     }
 }
